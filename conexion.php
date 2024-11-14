89a02897f8397cbaee100c5b8f01b8cc94dff88d
Raul Madero <r.madero.ramirez@gmail.com>
2024-11-14 14:35:14 -0600

    Cambio de conexion base de datos

--- conexion.php
+++ conexion.php
@@ -1,9 +1,9 @@
 <?php 
 	
-	$host = 'localhost';
-	$user = 'root';
-	$password = '';
-	$db = 'transvive';
+	$host = 'dpg-csr5lf1u0jms73cis3s0-a.oregon-postgres.render.com';
+	$user = 'rmadero';
+	$password = 'M6WeJ17g7uV3EsuLM1Ifjnt11HHf5i6y';
+	$db = 'transvive_piv3';
 
 	$conection = @mysqli_connect($host,$user,$password,$db);
 	$conection->set_charset('utf8');
--- sistema/db/datosConexion.php
+++ sistema/db/datosConexion.php
@@ -1,2 +1,2 @@
 <?php
-    $db = ['servidor' => 'localhost','usuario' => 'root', 'password' => '','db' => 'transvive'];
+    $db = ['servidor' => 'dpg-csr5lf1u0jms73cis3s0-a.oregon-postgres.render.com','rmadero' => 'root', 'password' => 'M6WeJ17g7uV3EsuLM1Ifjnt11HHf5i6y','db' => 'transvive_piv3'];
