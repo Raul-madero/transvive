49088d9877b98ce1c5a4febeca40a2312ad0d1d6
Raul Madero <r.madero.ramirez@gmail.com>
2024-11-27 11:09:35 -0600

    Correccion nginx.conf y archivo de conexion

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php 
 
-	$conection = mysqli_connect("127.0.0.1", "root", "Epasillas0406", "transvive", 3305);
-	// $conection = mysqli_connect($host, $user, $password, $database, $port);
+$conection = mysqli_connect("dpg-csr5lf1u0jms73cis3s0-a.oregon-postgres.render.com", "rmadero", "M6WeJ17g7uV3EsuLM1Ifjnt11HHf5i6y", "transvive_piv3", 5432) or
+mysqli_connect("127.0.0.1", "root", "Epasillas0406", "transvive", 3305);
 	$conection->set_charset('utf8');
 
 	if(!$conection){
