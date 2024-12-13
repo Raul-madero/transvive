4549953ac35a0a4b134a4a38e8082e6d248ca36a
raul.madero.ramirez <raul.madero.ramirez@gmail.com>
2024-12-13 17:35:50 +0000

    Insertar paginador orden de trabajo

--- sistema/orden_trabajo23.php
+++ sistema/orden_trabajo23.php
@@ -284,6 +284,22 @@ if (!$query_ordenes) {
   </div>
       <!-- /.row -->
     </section>
+    <div class="paginador">
+        <?php
+        if ($paginaActual > 1) {
+            echo '<a href="?pagina=' . ($paginaActual - 1) . '">Anterior</a>';
+        }
+
+        for ($i = 1; $i <= $totalPaginas; $i++) {
+            $claseActivo = $paginaActual == $i ? 'activo' : '';
+            echo '<a class="' . $claseActivo . '" href="?pagina=' . $i . '">' . $i . '</a>';
+        }
+
+        if ($paginaActual < $totalPaginas) {
+            echo '<a href="?pagina=' . ($paginaActual + 1) . '">Siguiente</a>';
+        }
+        ?>
+    </div>
     <!-- /.content -->
  </div>
   <!-- /.content-wrapper -->
