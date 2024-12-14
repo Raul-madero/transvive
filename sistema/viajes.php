b5ff335c4a077c776b4b60c96adf981f0bffb0f5
raul.madero.ramirez <raul.madero.ramirez@gmail.com>
2024-12-14 06:47:26 +0000

    Data no

--- sistema/viajes.php
+++ sistema/viajes.php
@@ -16,10 +16,6 @@ session_start();
 
     while ($data = mysqli_fetch_array($sql01)){
       $abiertos   = 0;
-      echo "<pre>";
-      print_r($data);
-      echo "<pre>";
-     
     }
      date_default_timezone_set('America/Mexico_City');
 
