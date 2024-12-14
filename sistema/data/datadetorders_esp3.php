46caffb9437f155cfe7814b64fa598dde55d2ee8
raul.madero.ramirez <raul.madero.ramirez@gmail.com>
2024-12-14 22:37:52 +0000

    cambio de optimizacion

--- sistema/data/datadetorders_esp3.php
+++ sistema/data/datadetorders_esp3.php
@@ -1,153 +1,121 @@
 <?php
 session_start();
- $User=$_SESSION['user'];
- $rol=$_SESSION['rol'];
- $idUser = $_SESSION['idUser'];
+$User = $_SESSION['user'];
+$rol = $_SESSION['rol'];
+$idUser = $_SESSION['idUser'];
+
 include '../config/db-config.php';
 
+if ($_REQUEST['action'] == 'fetch_users') {
+    global $connection;
 
-global $connection;
-
-if($_REQUEST['action'] == 'fetch_users'){
-
+    // Validar y limpiar entradas
     $requestData = $_REQUEST;
-    $start = $_REQUEST['start'];
+    $start = intval($requestData['start']);
+    $length = intval($requestData['length']);
+    $searchValue = isset($requestData['search']['value']) ? mysqli_real_escape_string($connection, $requestData['search']['value']) : '';
+    $initial_date = !empty($requestData['initial_date']) ? mysqli_real_escape_string($connection, $requestData['initial_date']) : null;
+    $final_date = !empty($requestData['final_date']) ? mysqli_real_escape_string($connection, $requestData['final_date']) : null;
+    $gender = isset($requestData['gender']) ? mysqli_real_escape_string($connection, $requestData['gender']) : '';
 
-    $initial_date = $_REQUEST['initial_date'];
-    $final_date = $_REQUEST['final_date'];
-    $gender = $_REQUEST['gender'];
+    // Condiciones dinámicas
+    $conditions = [
+        "p.tipo_viaje LIKE '%Especial%'",
+        "p.id_supervisor = $idUser"
+    ];
 
-    if(!empty($initial_date) && !empty($final_date)){
-        $date_range = " AND p.fecha BETWEEN '".$initial_date."' AND '".$final_date."' ";
-    }else{
-        $date_range = "";
+    if ($initial_date && $final_date) {
+        $conditions[] = "p.fecha BETWEEN '$initial_date' AND '$final_date'";
     }
 
-    if($gender != ""){
-      
-         $gender = " AND p.id = '$gender' ";
-       
+    if ($gender !== '') {
+        $conditions[] = "p.id = '$gender'";
     }
 
-  
+    // Filtrar por búsqueda
+    if (!empty($searchValue)) {
+        $searchConditions = [
+            "cliente LIKE '%$searchValue%'",
+            "p.id LIKE '%$searchValue%'",
+            "tipo_viaje LIKE '%$searchValue%'",
+            "direccion LIKE '%$searchValue%'",
+            "destino LIKE '%$searchValue%'"
+        ];
+        $conditions[] = '(' . implode(' OR ', $searchConditions) . ')';
+    }
 
-    $columns = ' p.id, p.fecha, p.hora_inicio, p.hora_fin, p.semana, p.cliente, p.operador, p.unidad, p.num_unidad, p.personas, p.estatus, CONCAT(sp.nombres, " ", sp.apellido_paterno, " ", apellido_materno) as name, us.nombre AS jefeo, p.ruta, p.direccion, p.destino, p.costo_viaje, p.sueldo_vuelta, tipo_viaje ';
-    $table = ' registro_viajes p LEFT JOIN clientes ct ON p.cliente=ct.nombre_corto LEFT JOIN usuario us ON ct.id_supervisor = us.idusuario LEFT JOIN supervisores sp ON p.id_supervisor = sp.idacceso' ;
-    $where = " WHERE p.tipo_viaje like '%Especial%' and p.id_supervisor = $idUser ".$date_range.$gender ;
+    // Construir consulta
+    $where = "WHERE " . implode(' AND ', $conditions);
 
-    $columns_order = array(
-        0 => 'id',
-        1 => 'fecha',
-        2 => 'cliente',
-        3 => 'direccion',
-        4 => 'hora_inicio',
-        5 => 'hora_fin',
-        6 => 'unidad',
-        7 => 'destino',
-        8 => 'jefeo',
-        9 => 'estatus'
-    );
+    $columns = "
+        p.id, p.fecha, p.hora_inicio, p.hora_fin, p.semana, p.cliente, p.operador, p.unidad, 
+        p.num_unidad, p.personas, p.estatus, 
+        CONCAT(sp.nombres, ' ', sp.apellido_paterno, ' ', sp.apellido_materno) AS name, 
+        us.nombre AS jefeo, p.ruta, p.direccion, p.destino, p.costo_viaje, 
+        p.sueldo_vuelta, tipo_viaje
+    ";
 
-    $sql = "SELECT ".$columns." FROM ".$table." ".$where;
+    $table = "
+        registro_viajes p
+        LEFT JOIN clientes ct ON p.cliente = ct.nombre_corto
+        LEFT JOIN usuario us ON ct.id_supervisor = us.idusuario
+        LEFT JOIN supervisores sp ON p.id_supervisor = sp.idacceso
+    ";
 
+    $orderColumnIndex = intval($requestData['order'][0]['column']);
+    $orderDirection = $requestData['order'][0]['dir'] === 'asc' ? 'ASC' : 'DESC';
+    $columnsOrder = ['id', 'fecha', 'cliente', 'direccion', 'hora_inicio', 'hora_fin', 'unidad', 'destino', 'jefeo', 'estatus'];
+    $orderBy = $columnsOrder[$orderColumnIndex] ?? 'id';
+
+    $sql = "SELECT $columns FROM $table $where";
+    $totalResult = mysqli_query($connection, $sql);
+    $totalFiltered = mysqli_num_rows($totalResult);
+
+    $sql .= " ORDER BY $orderBy $orderDirection LIMIT $start, $length";
     $result = mysqli_query($connection, $sql);
-    $totalData = mysqli_num_rows($result);
-    $totalFiltered = $totalData;
 
-    if( !empty($requestData['search']['value']) ) {
-        $sql.="AND ( cliente LIKE '%".$requestData['search']['value']."%' ";
-        $sql.=" OR p.id LIKE '%".$requestData['search']['value']."%' ";
-        $sql.=" OR tipo_viaje LIKE '%".$requestData['search']['value']."%' ";
-        $sql.=" OR direccion LIKE '%".$requestData['search']['value']."%' ";
-        $sql.=" OR destino LIKE '%".$requestData['search']['value']."%'  )";
-       
-        
+    // Procesar datos
+    $data = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $statusMap = [
+            1 => '<span class="label label-primary">Activo</span>',
+            2 => '<span class="label label-success">Realizado</span>',
+            3 => '<span class="label label-danger">Cancelado</span>',
+            4 => '<span class="label label-primary">Iniciado</span>',
+            5 => '<span class="label label-info">Terminado</span>',
+            'default' => '<span class="label label-success">CERRADO</span>'
+        ];
+
+        $Estatusnew = $statusMap[$row['estatus']] ?? $statusMap['default'];
+
+        $data[] = [
+            'counter' => ++$start,
+            'pedidono' => $row['id'],
+            'nopedido' => '<a style="text-decoration:none" href="factura/pedidonw.php?id=' . $row['id'] . '" target="_blank">' . $row['id'] . '</a>',
+            'fechaa' => date('d/m/Y', strtotime($row['fecha'])),
+            'horainicio' => date('H:i', strtotime($row['hora_inicio'])),
+            'horafin' => date('H:i', strtotime($row['hora_fin'])),
+            'nosemana' => $row['semana'],
+            'razonsocial' => $row['cliente'],
+            'rutacte' => $row['ruta'],
+            'supervisor' => $row['name'],
+            'jefeopera' => $row['jefeo'],
+            'origen' => $row['direccion'],
+            'Destino' => $row['destino'],
+            'Costo' => $row['costo_viaje'],
+            'Valor_vuelta' => $row['sueldo_vuelta'],
+            'TipoViaje' => $row['tipo_viaje'],
+            'estatusped' => $Estatusnew
+        ];
     }
 
-    $result = mysqli_query($connection, $sql);
-    $totalData = mysqli_num_rows($result);
-    $totalFiltered = $totalData;
-
-    $sql .= " ORDER BY ". $columns_order[$requestData['order'][0]['column']]."   ".$requestData['order'][0]['dir'];
-
-    if($requestData['length'] != "-1"){
-        $sql .= " LIMIT ".$requestData['start']." ,".$requestData['length'];
-    }
-
-    $result = mysqli_query($connection, $sql);
-    $data = array();
-    $counter = $start;
-
-    $count = $start;
-    while($row = mysqli_fetch_array($result)){
-        if ($row['estatus'] == 1){
-        $Estatusnew = '<span class="label label-primary">Activo</span>'; 
-    }else{
-        if ($row['estatus'] == 2){
-           $Estatusnew = '<span class="label label-success">Realizado</span>';
-        }else{
-            if ($row['estatus'] == 3) {
-              $Estatusnew = '<span class="label label-danger">Cancelado</span>';
-            }else {
-                if ($row['estatus'] == 4) {
-                 $Estatusnew = '<span class="label label-primary">Iniciado</span>';
-                }else {
-                 if ($row['estatus'] == 5) {
-                  $Estatusnew = '<span class="label label-info">Terminado</span>';
-                 }else {
-                  $Estatusnew = '<span class="label label-success">CERRADO</span>';
-                 } 
-                }     
-        }
-    }
-    }
-
-        $count++;
-        $nestedData = array();
-
-        $nestedData['counter'] = $count;
-        $nestedData['pedidono'] =  $row["id"];
-
-        $nestedData['nopedido'] = '<a style="text-decoration:none" href="factura/pedidonw.php?id='.($row["id"]).'" target="_blank">'.($row["id"]).'</a>';
-        $time = strtotime($row["fecha"]);
-        $time2 = strtotime($row["hora_inicio"]);
-        $time3 = strtotime($row["hora_fin"]);
-        $nestedData['fechaa'] = date('d/m/Y', $time);
-        $nestedData['horainicio'] = date('H:i', $time2);
-        $nestedData['horafin'] = date('H:i', $time3);
-        $nestedData['nosemana'] = $row["semana"];
-
-        $nestedData['razonsocial'] = $row["cliente"];
-        $nestedData['rutacte'] = $row["ruta"];
-        $time2 = strtotime($row["fechacomp"]);
-        $nestedData['fechacomp'] = date('d M, Y', $time);
-        
-        $nestedData['conductor'] = $row["operador"];
-        $nestedData['tipounidad'] = $row["unidad"];
-        $nestedData['nounidad'] = $row["num_unidad"];
-        $nestedData['supervisor'] = $row["name"];
-        $nestedData['jefeopera'] = $row["jefeo"];
-        $nestedData['origen'] = $row["direccion"];
-        $nestedData['Destino'] = $row["destino"];
-        $nestedData['Costo'] = $row["costo_viaje"];
-        $nestedData['Valor_vuelta'] = $row["sueldo_vuelta"];
-        $nestedData['Datenew'] = $row["fecha"];
-        $nestedData['TipoViaje'] = $row["tipo_viaje"];
-
-        $nestedData['estatusped'] = $Estatusnew;
-
-        $data[] = $nestedData;
-    }
-    header('Content-Type: application/json charset=utf-8');
-    
-    $json_data = array(
-        "draw"            => intval( $requestData['draw'] ),
-        "recordsTotal"    => intval( $totalData),
-        "recordsFiltered" => intval( $totalFiltered ),
-        "records"         => $data
-    );
-
-    echo json_encode($json_data);
+    // Respuesta JSON
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode([
+        "draw" => intval($requestData['draw']),
+        "recordsTotal" => intval($totalFiltered),
+        "recordsFiltered" => intval($totalFiltered),
+        "records" => $data
+    ]);
 }
-
 ?>
