08ba82fe39b8e154634ef1d39a8d86bc04b055da
raul.madero.ramirez <raul.madero.ramirez@gmail.com>
2024-12-16 04:54:13 +0000

    Comillas

--- sistema/import_carga_planeacion.php
+++ sistema/import_carga_planeacion.php
@@ -29,7 +29,7 @@ if (isset($_FILES['name']) && $_FILES['name']['error'] === UPLOAD_ERR_OK) {
 
     if ($up->uploaded) {
         $up->Process("./");
-        echo <script> alert('subido'); </script>;
+        echo "<script> alert('subido'); </script>";
 
         if ($up->processed) {
             $file_path = "./" . $up->file_dst_name;
