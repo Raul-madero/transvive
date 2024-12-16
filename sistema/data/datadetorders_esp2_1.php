4343ebdb8971e72589605c2273ad0d9e1024cca6
raul.madero.ramirez <raul.madero.ramirez@gmail.com>
2024-12-16 20:25:19 +0000

    paginador

--- sistema/data/datadetorders_esp2_1.php
+++ sistema/data/datadetorders_esp2_1.php
@@ -50,6 +50,26 @@ if ($_REQUEST['action'] == 'fetch_users'){
     $totalData = mysqli_num_rows($result);
     $totalFiltered = $totalData;
 
+     // Obtener el total de registros sin LIMIT (para el conteo total de páginas)
+     $sql_total = "SELECT COUNT(*) FROM " . $table . $where;
+     $result_total = mysqli_query($connection, $sql_total);
+     $row_total = mysqli_fetch_array($result_total);
+     $totalData = $row_total[0];
+     
+     // Consulta con LIMIT y OFFSET para la paginación
+     $sql = "SELECT " . $columns . " FROM " . $table . $where;
+     if (!empty($requestData['order'][0]['column'])) {
+         $sql .= " ORDER BY " . $columns_order[$requestData['order'][0]['column']] . " " . $requestData['order'][0]['dir'];
+     }
+     if ($length != "-1") {
+         $sql .= " LIMIT " . $start . ", " . $length;
+     }
+
+     $result = mysqli_query($connection, $sql);
+     $totalData = mysqli_num_rows($result);
+     $totalFiltered = $totalData;
+ 
+
     $data = array();
     $counter = $start;
 
@@ -67,7 +87,6 @@ if ($_REQUEST['action'] == 'fetch_users'){
             $sql .= " LIMIT " . $requestData['start'] . " ," . $requestData['length'];
         }
 
-        
 
         $result = mysqli_query($connection, $sql);
         $totalData = mysqli_num_rows($result);
