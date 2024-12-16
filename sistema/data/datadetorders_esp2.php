49284426bf06d30f8825191af8dc7a85bcb9b588
raul.madero.ramirez <raul.madero.ramirez@gmail.com>
2024-12-16 15:00:18 +0000

    Viake

--- sistema/data/datadetorders_esp2.php
+++ sistema/data/datadetorders_esp2.php
@@ -24,9 +24,16 @@ if($_REQUEST['action'] == 'fetch_users'){
         $gender = " AND YEAR(p.fecha) = '$gender' ";
     }
 
-    $columns = ' p.id, p.fecha, p.hora_inicio, p.hora_fin, p.semana, p.cliente, p.operador, p.unidad, p.num_unidad, p.personas, p.estatus, CONCAT(sp.nombres, " ", sp.apellido_paterno, " ", apellido_materno) as name, us.nombre AS jefeo, p.ruta, p.direccion, p.destino, p.costo_viaje, p.sueldo_vuelta, p.numero_unidades, p.tipo_viaje ';
-    $table = ' registro_viajes p LEFT JOIN clientes ct ON p.cliente=ct.nombre_corto LEFT JOIN usuario us ON ct.id_supervisor = us.idusuario LEFT JOIN supervisores sp ON p.id_supervisor = sp.idacceso' ;
-    $where = " WHERE p.tipo_viaje LIKE '%Especial%' "
+    $columns = ' p.id, p.fecha, p.hora_inicio, p.hora_fin, p.semana, p.cliente, p.operador, p.unidad, 
+    p.num_unidad, p.personas, p.estatus, 
+    CONCAT(sp.nombres, ' ', sp.apellido_paterno, ' ', sp.apellido_materno) AS name, 
+    us.nombre AS jefeo, p.ruta, p.direccion, p.destino, p.costo_viaje, 
+    p.sueldo_vuelta, tipo_viaje';
+    $table = ' registro_viajes p 
+    LEFT JOIN clientes ct ON p.cliente=ct.nombre_corto 
+    LEFT JOIN usuario us ON ct.id_supervisor = us.idusuario
+    LEFT JOIN supervisores sp ON p.id_supervisor = sp.idacceso' ;
+    // $where = " WHERE p.tipo_viaje LIKE '%Especial%' "
     // .$date_range.$gender ;
 
     $columns_order = array(
