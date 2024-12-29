4a5f113365a962a11b541361f27334e5643a51db
raul.madero.ramirez <raul.madero.ramirez@gmail.com>
2024-12-29 02:02:14 +0000

    query

--- sistema/includes/ajax.php
+++ sistema/includes/ajax.php
@@ -1222,9 +1222,7 @@ if($_POST['action'] == 'AlmacenaViaje')
     
             $query_procesar = mysqli_query($conection,"CALL procesar_editempleado($id, $noempleado, '$name', '$paterno', '$materno', '$cargo', '$telefono', '$rfc', '$unidad', '$nounidad', '$tipo_lic', '$nolicencia', '$fecha_vence', '$supervisor', '$tipocontrato', '$contrato', '$fincontrato', '$imss', $salariodia, $sueldobase, $sueldo, $sueldob2, $vdgmv, $vdgao, $sprinter, $sauto, $ssemi, $deuda, $descuento, $adeudo, $saldo_adeudo, $bono, '$clasif_cat', $bonoc2, $bonosemanal, $apoyomes, $vales, $caja, $vacaciones, $efectivo, $descfiscal, '$tipo_nomina', '$sexo', '$fechanac', $edad, '$edocivil', '$domicilio', '$estudios', '$contactoe', '$elcurp', '$fchaaltaimss', '$noss', $salarioxdia, $sueldoauto, $sdosprinter, '$es_recontrata', '$recontratable', '$comentarios', '$datebaja', '$datereingreso', $usuario)");
 
-            echo $query_procesar;
-
-            $result_detalle = mysqli_affected_rows($conection);
+            $result_detalle = mysqli_affected_rows($query_procesar);
             
             if($result_detalle > 0){
                 $data = mysqli_fetch_assoc($conection);
