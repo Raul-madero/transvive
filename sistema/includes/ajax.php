3b00121a113bb2a8fefad78c66090341e92c53c7
raul.madero.ramirez <raul.madero.ramirez@gmail.com>
2024-12-31 17:21:45 +0000

    Modificacion parametros

--- sistema/includes/ajax.php
+++ sistema/includes/ajax.php
@@ -2925,24 +2925,24 @@ if($_POST['action'] == 'AlmacenaEditSolicitudmantto')
        echo 'error';
     }else{
         
-        $folio        = $_POST['folio'] ? $_POST['folio'] : null;
-        $fecha        = $_POST['fecha'] ? $_POST['fecha'] : null;
-        $nounidad     = $_POST['nounidad'] ? $_POST['nounidad'] : null;
-        $tipo_unidad  = $_POST['tipo_unidad'] ? $_POST['tipo_unidad'] : null;
-        $operador     = $_POST['operador'] ? $_POST['operador'] : null;
-        $solicita     = $_POST['solicita'] ? $_POST['solicita'] : null;
-        $tipo_trab    = $_POST['tipotrabajo'] ? $_POST['tipotrabajo'] : null;
-        $kmneumatico  = $_POST['kmneumatico'] ? $_POST['kmneumatico'] : null;
-        $tipo_mantto  = $_POST['tipomantto'] ? $_POST['tipomantto'] : null;
-        $programado   = $_POST['programado'] ? $_POST['programado'] : null;
-        $trabajo_sol  = $_POST['trabajosolic'] ? $_POST['trabajosolic'] : null;
-        $trabajohecho = $_POST['trabajohecho'] ? $_POST['trabajohecho'] : null;
-        $costos_desc  = $_POST['costosdesc'] ? $_POST['costodesc'] : null;
-        $fechaini     = $_POST['fechaini'] ? $_POST['fechaini'] : null;
-        $fechafin     = $_POST['fechafin'] ? $_POST['fechafin'] : null;
-        $notas        = $_POST['notas'] ? $_POST['notas'] : null;
-        $notas_genera = $_POST['notas_genera'] ? $_POST['notas_genera'] : null;
-        $causas       = $_POST['causas'] ? $_POST['causas'] : null;
+        $folio        = (isset($_POST['folio'])) ? $_POST['folio'] : null;
+        $fecha        = (isset($_POST['fecha'])) ? $_POST['fecha'] : null;
+        $nounidad     = (isset($_POST['nounidad'])) ? $_POST['nounidad'] : null;
+        $tipo_unidad  = (isset($_POST['tipo_unidad'])) ? $_POST['tipo_unidad'] : null;
+        $operador     = (isset($_POST['operador'])) ? $_POST['operador'] : null;
+        $solicita     = (isset($_POST['solicita'])) ? $_POST['solicita'] : null;
+        $tipo_trab    = (isset($_POST['tipotrabajo'])) ? $_POST['tipotrabajo'] : null;
+        $kmneumatico  = (isset($_POST['kmneumatico'])) ? $_POST['kmneumatico'] : null;
+        $tipo_mantto  = (isset($_POST['tipomantto'])) ? $_POST['tipomantto'] : null;
+        $programado   = (isset($_POST['programado'])) ? $_POST['programado'] : null;
+        $trabajo_sol  = (isset($_POST['trabajosolic'])) ? $_POST['trabajosolic'] : null;
+        $trabajohecho = (isset($_POST['trabajohecho'])) ? $_POST['trabajohecho'] : null;
+        $costos_desc  = (isset($_POST['costosdesc'])) ? $_POST['costodesc'] : null;
+        $fechaini     = (isset($_POST['fechaini'])) ? $_POST['fechaini'] : null;
+        $fechafin     = (isset($_POST['fechafin'])) ? $_POST['fechafin'] : null;
+        $notas        = (isset($_POST['notas'])) ? $_POST['notas'] : null;
+        $notas_genera = (isset($_POST['notas_genera'])) ? $_POST['notas_genera'] : null;
+        $causas       = (isset($_POST['causas'])) ? $_POST['causas'] : null;
 
         $token       = md5($_SESSION['idUser']);
         $usuario     = $_SESSION['idUser'];
