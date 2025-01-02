8564afc20b8e6c0900940eeea08343f18cf5866f
raul.madero.ramirez <raul.madero.ramirez@gmail.com>
2025-01-02 14:29:22 +0000

    initial date

--- sistema/viajes_especiales23.php
+++ sistema/viajes_especiales23.php
@@ -524,9 +524,9 @@ session_start();
               dataType: 'json',
               data: function(d) {
                 d.action = "fetch_users"
-                d.initial_date = $("#initial_date").val() || ""
-                d.final_date = $("#final_date").val() || ""
-                d.gender = $("#gender").val() || ""
+                d.initial_date = $("#initial_date").val()
+                d.final_date = $("#final_date").val()
+                d.gender = $("#gender").val()
                 if (d.order.length > 0) {
                   d.order_column = d.columns[d.order[0].column].data;
                   d.order_dir = d.order[0].dir;
