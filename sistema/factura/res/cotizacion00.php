3ddbe507fd0a53183cc3834125036ff41aaa1451
raul.madero.ramirez <raul.madero.ramirez@gmail.com>
2025-01-09 18:47:54 +0000

    espacio

--- sistema/factura/res/cotizacion00.php
+++ sistema/factura/res/cotizacion00.php
@@ -65,7 +65,7 @@ if ($pedido['fecha_fin'] > '2000-01-01') {
  <p>   
     <tr>
   
-  <td rowspan="1" colspan="4" style="width: 100%; font-size: 10pt;" align="right">&nbsp;<?php echo 'Guadalajara, Jal. a '. $dia . 'de ' . $mes . 'de ' . $anio ?></td>
+  <td rowspan="1" colspan="4" style="width: 100%; font-size: 10pt;" align="right">&nbsp;<?php echo 'Guadalajara, Jal. a '. $dia . ' de ' . $mes . ' de ' . $anio ?></td>
   <br>
  
   
