4c845dda338eadc155b426d7181b9bd43e16a8f9
raul.madero.ramirez <raul.madero.ramirez@gmail.com>
2025-01-09 18:29:27 +0000

    fecha mayusculas

--- sistema/factura/res/cotizacion00.php
+++ sistema/factura/res/cotizacion00.php
@@ -52,7 +52,7 @@ if ($pedido['fecha_fin'] > '2000-01-01') {
 
  <!--<tr>
   
-  <td rowspan="1" colspan="4" style="width: 100%; font-size: 10pt;" align="right">&nbsp;<?php echo 'Guadalajara, Jal. a '.$mesDesc ?></td>
+  <td rowspan="1" colspan="4" style="width: 100%; font-size: 10pt;" align="right">&nbsp;<?php echo 'Guadalajara, Jal. a '. strtoupper($mesDesc) ?></td>
   <br>
  
   
