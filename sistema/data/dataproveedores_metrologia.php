5950b9afd578c2b60835cf10a680607f2396fca8
raul.madero.ramirez <raul.madero.ramirez@gmail.com>
2025-01-15 16:09:35 +0000

    evaluaciones proveedor

--- sistema/data/dataproveedores_metrologia.php
+++ sistema/data/dataproveedores_metrologia.php
@@ -1,18 +1,18 @@
 <?php
 session_start();
-include '../config/db-config.php';
+include '../../conexion.php';
 
 
-global $connection;
+global $conection;
 
 if($_REQUEST['action'] == 'fetch_users'){
 
     $requestData = $_REQUEST;
     $start = $_REQUEST['start'];
 
-    $initial_date = $_REQUEST['initial_date'];
-    $final_date = $_REQUEST['final_date'];
-    $gender = $_REQUEST['gender'];
+    $initial_date = $_REQUEST['initial_date'] ?? "";
+    $final_date = $_REQUEST['final_date'] ?? "";
+    $gender = $_REQUEST['gender'] ?? "";
 
     if(!empty($initial_date) && !empty($final_date)){
         $date_range = " AND p.fecha BETWEEN '".$initial_date."' AND '".$final_date."' ";
@@ -42,7 +42,7 @@ if($_REQUEST['action'] == 'fetch_users'){
 
     $sql = "SELECT ".$columns." FROM ".$table." ".$where;
 
-    $result = mysqli_query($connection, $sql);
+    $result = mysqli_query($conection, $sql);
     $totalData = mysqli_num_rows($result);
     $totalFiltered = $totalData;
 
@@ -53,7 +53,7 @@ if($_REQUEST['action'] == 'fetch_users'){
         
     }
 
-    $result = mysqli_query($connection, $sql);
+    $result = mysqli_query($conection, $sql);
     $totalData = mysqli_num_rows($result);
     $totalFiltered = $totalData;
 
@@ -63,7 +63,7 @@ if($_REQUEST['action'] == 'fetch_users'){
         $sql .= " LIMIT ".$requestData['start']." ,".$requestData['length'];
     }
 
-    $result = mysqli_query($connection, $sql);
+    $result = mysqli_query($conection, $sql);
     $data = array();
     $counter = $start;
 
