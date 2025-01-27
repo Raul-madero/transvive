1b262af1986d92a1d63d00327e65502cdc082ad0
raul.madero.ramirez <raul.madero.ramirez@gmail.com>
2025-01-27 23:31:40 +0000

    Regreso al codigo

--- sistema/import_alertas.php
+++ sistema/import_alertas.php
@@ -3,48 +3,49 @@ include "../conexion.php";
 include "class.upload.php";
 session_start();
 
-$usuario = $_SESSION['idUser'];
+$usuario     = $_SESSION['idUser'];
 
-if (isset($_FILES["name"])) {
-    $up = new Upload($_FILES["name"]);
-    if ($up->uploaded) {
-        $up->Process("./");
-        if ($up->processed) {
+if(isset($_FILES["name"])){
+	$up = new Upload($_FILES["name"]);
+	if($up->uploaded){
+		$up->Process("./");
+		if($up->processed){
+if ( $file = fopen( "./" . $up->file_dst_name , "r" ) ) {
 
-            $ok = 0;
-            $error = 0;
+$ok = 0;
+$error = 0;
+$products_array = array();
 
-            if ($file = fopen("./" . $up->file_dst_name, "r")) {
-                while ($x = fgets($file, 4096)) {
-                    $data = explode(",", $x);
-                    if (count($data) >= 6) {
-                        
-                        $semana = trim(str_replace(array("\r", "\n", "\t"), '', $data[5])); // Limpiar la columna 'semana'
-                        $sql = "INSERT INTO alertas (semana, unidad, operador, noalertas, velocidad, limite, user_id) 
-                                VALUES ('$semana', '$data[0]', '$data[1]', '$data[2]', '$data[3]', '$data[4]', '$usuario')";
-								echo $sql;
-								exit;
-                        if ($conection->query($sql)) {
-							$ok++;
-                            
-                        } else {
-                            // Si la inserción falló, registra el error o muestra un mensaje de error
-                            $error++;
-                        }
-                    } else {
-                        $error++;
-                    }
-                }
-            }
-
-            fclose($file);
-            unlink("./" . $up->file_dst_name);
-        }
+    while($x=fgets($file,4096)){
+    	
+    		$data = explode(",", $x);
+    		if(count($data)>=6){
+    			$ok++;
+    			$sql = "insert into alertas (semana,unidad,operador,noalertas,velocidad,limite,user_id) value (\"$data[5]\",\"$data[0]\",\"$data[1]\",\"$data[2]\",\"$data[3]\",\"$data[4]\",\"$usuario\")";
+				echo $sql;
+				exit;
+    			$conection->query($sql);
+    		}else{
+    			$error++;
+    		}
     }
 }
 
+    $sql3 = "UPDATE alertas SET semana = REPLACE(REPLACE(REPLACE(semana,CHAR(9),''),CHAR(10),''),CHAR(13),'')";
+         
+    $conection->query($sql3);
+
+
+        fclose($file);
+		unlink("./".$up->file_dst_name);
+	}
+	
+}
+
+}
 echo "<script>
 alert('Correcto $ok, Error $error !!!');
 window.location = './alertas.php';
-</script>";
+</script>
+";
 ?>
