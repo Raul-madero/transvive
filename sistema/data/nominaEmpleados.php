b1309a4fe9d4f9783f8760e219522ba3429c48a0
raul.madero.ramirez <raul.madero.ramirez@gmail.com>
2025-01-31 21:09:17 +0000

    alertas

--- sistema/data/nominaEmpleados.php
+++ sistema/data/nominaEmpleados.php
@@ -141,9 +141,9 @@ if(isset($_POST['semana']) && isset($_POST['anio']) && !empty($_POST['semana']) 
         $sql_empleados .= "
             FROM 
                 empleados e
-                LEFT JOIN 
+            LEFT JOIN 
                 alertas al ON al.operador = CONCAT(e.nombres, ' ', e.apellido_paterno, ' ', e.apellido_materno) 
-                            AND al.fecha = DATE_ADD('2025-01-20', INTERVAL (7 - WEEKDAY('2025-01-20')) DAY)
+                            AND al.fecha BETWEEN '$fecha_inicio' AND '$fecha_fin'
             LEFT JOIN 
                 incidencias inc ON inc.empleado = CONCAT(e.nombres, ' ', e.apellido_paterno, ' ', e.apellido_materno) AND inc.nodesemana = '$nombre_semana'
             LEFT JOIN 
