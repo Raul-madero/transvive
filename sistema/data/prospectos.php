05faad6573b17da60015592358d63880517610c4
raul.madero.ramirez <raul.madero.ramirez@gmail.com>
2025-02-04 00:47:01 +0000

    Tipos unidad, origen, semaforo

--- sistema/data/prospectos.php
+++ sistema/data/prospectos.php
@@ -10,6 +10,22 @@ $sql = "SELECT * FROM prospectos";
 $query = mysqli_query($conection, $sql);
 $data = array();
 while($row = mysqli_fetch_assoc($query)) {
+    $unidad = isset($row['unidad']) ? $row['unidad'] : 1;
+    $origen = isset($row['origen']) ? $row['origen'] : 1;
+    $semaforo = isset($row['semaforo']) ? $row['semaforo'] : 1;
+
+
+    $sql_tipo_unidad = "SELECT unidad FROM tipo_unidad WHERE id = " . $unidad;
+    $query_tipo_unidad = mysqli_query($conection, $sql_tipo_unidad);
+    $row['tipo_unidad'] = mysqli_fetch_assoc($query_tipo_unidad);
+
+    $sql_origen = "SELECT origen FROM origen_prospecto WHERE id = " . $origen;
+    $query_origen = mysqli_query($conection, $sql_origen);
+    $row['origen'] = mysqli_fetch_assoc($query_origen);
+
+    $sql_semaforo = "SELECT semaforo FROM semaforo WHERE id = " . $semaforo;
+    $query_semaforo = mysqli_query($conection, $sql_semaforo);
+    $row['semaforo'] = mysqli_fetch_assoc($query_semaforo);
     $data[] = $row;
 }
 // var_dump($data);
