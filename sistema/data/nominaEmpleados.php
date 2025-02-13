98eb80361cd7ed5fa3cd8a1c620bd0316a10cb4d
raul.madero.ramirez <raul.madero.ramirez@gmail.com>
2025-02-13 18:44:41 +0000

    vardump

--- sistema/data/nominaEmpleados.php
+++ sistema/data/nominaEmpleados.php
@@ -219,7 +219,7 @@ if(isset($_POST['semana']) && isset($_POST['anio']) && !empty($_POST['semana']) 
         }
         // Solo insertar registros si no existen datos para la semana y el año
         while ($row_empleados = mysqli_fetch_assoc($result_empleados)) {
-            // var_dump($row_empleados);
+            var_dump($row_empleados);
             $alertas = intval($row_empleados['noalertas']);
             $bono_semanal_contrato = calcularBonoSemanalContrato($row_empleados['fecha_contrato'], $fecha_fin);
             $gana_bono = ($alertas < 5 && $bono_semanal_contrato) ? true : false;
@@ -253,7 +253,7 @@ if(isset($_POST['semana']) && isset($_POST['anio']) && !empty($_POST['semana']) 
             $dias_vacaciones = isset($row_empleados['fecha_final']) || isset($row_empleados['fecha_inicial']) ? (intval($row_empleados['fecha_inicial']) + 1) ?? (intval($row_empleados['fecha_final']) + 1) : 0;
             $pago_vacaciones = ($dias_vacaciones * $row_empleados['salario_diario']) ?? 0;
             $bono_categoria = dia15EntreFechas($fecha_inicio, $fecha_fin) ? floatval($row_empleados['bono_categoria']) : 0;
-            $bono_semanal = ($gana_bono && $dias_vacaciones === 0 && $bono_semanal_contrato) ? floatval($row_empleados['bono_semanal']) : 0;
+            $bono_semanal = ($gana_bono && $dias_vacaciones == 0 && $bono_semanal_contrato) ? floatval($row_empleados['bono_semanal']) : 0;
             $neto = ($cargo == 'OPERADOR') ? ($sueldo_bruto + $bono_categoria + $bono_semanal + $row_empleados['bono_supervisor'] + $pago_vacaciones + $prima_vacacional - $pago_fiscal - $deducciones - $caja_ahorro + $apoyo_mes) : ($bono_categoria + $bono_semanal + $row_empleados['bono_supervisor'] + $pago_vacaciones + $prima_vacacional + $pago_fiscal - $deducciones - $caja_ahorro + $apoyo_mes - $deduccion_fiscal);
     
             // Preparar los datos para la inserción
