7c75488f1a06d9fa4809f21a12aacdb4c62a8211
raul.madero.ramirez <raul.madero.ramirez@gmail.com>
2025-02-28 22:46:22 +0000

    horarios

--- sistema/includes/ajax.php
+++ sistema/includes/ajax.php
@@ -2488,11 +2488,11 @@ if ($_POST['action'] == 'AlmacenaRuta') {
         $ruta       = mysqli_real_escape_string($conection, trim($_POST['ruta']));
         $noeco      = mysqli_real_escape_string($conection, trim($_POST['noeconomico']));
         $operador   = mysqli_real_escape_string($conection, trim($_POST['operador']));
-        $horario1   = mysqli_real_escape_string($conection, trim($_POST['horario1']))?? '00:00:00';
-        $horario2   = mysqli_real_escape_string($conection, trim($_POST['horario2'])) ?? '00:00:00';
-        $horario3   = mysqli_real_escape_string($conection, trim($_POST['horario3'])) ?? '00:00:00';
-        $hmixto1    = mysqli_real_escape_string($conection, trim($_POST['hmixto1'])) ?? '00:00:00';
-        $hmixto2    = mysqli_real_escape_string($conection, trim($_POST['hmixto2'])) ?? '00:00:00';
+        $horario1 = !empty($_POST['horario1']) ? "'".$_POST['horario1']."'" : "NULL";
+        $horario2 = !empty($_POST['horario2']) ? "'".$_POST['horario2']."'" : "NULL";
+        $horario3 = !empty($_POST['horario3']) ? "'".$_POST['horario3']."'" : "NULL";
+        $hmixto1  = !empty($_POST['hmixto1'])  ? "'".$_POST['hmixto1']."'"  : "NULL";
+        $hmixto2  = !empty($_POST['hmixto2'])  ? "'".$_POST['hmixto2']."'"  : "NULL";
         $diasviajes = mysqli_real_escape_string($conection, trim($_POST['diasviajes']));
         $sueldo_vta = !empty($_POST['sueldo_vta']) ? floatval($_POST['sueldo_vta']) : 'NULL';
         $sueldo_vtaneta = !empty($_POST['sueldo_vtaneta']) ? floatval($_POST['sueldo_vtaneta']) : 'NULL';
