a83136cde42dfcddbbe251cdd7a9948228eb345f
Raul Madero <raul.madero.ramirez@gmail.com>
2025-03-03 15:38:30 -0600

    Fecha inicio fecha fin incidencias

--- sistema/data/nominaEmpleados.php
+++ sistema/data/nominaEmpleados.php
@@ -218,8 +218,7 @@ if(isset($_POST['semana']) && isset($_POST['anio']) && !empty($_POST['semana']) 
     LEFT JOIN alertas al ON al.operador = CONCAT_WS(' ', e.nombres, e.apellido_paterno, e.apellido_materno) COLLATE utf8mb4_general_ci
         AND DATE(al.fecha) BETWEEN '$fecha_fin' AND '$fecha_limite_alertas'
     LEFT JOIN incidencias inc ON inc.empleado = CONCAT_WS(' ', e.nombres, e.apellido_paterno, e.apellido_materno)
-        AND inc.fecha_inicial BETWEEN '$fecha_inicio' AND '$fecha_fin'
-        AND inc.fecha_final BETWEEN '$fecha_inicio' AND '$fecha_fin'
+        AND inc.fecha_inicial BETWEEN '$fecha_inicio' AND '$fecha_fin' OR inc.fecha_final BETWEEN '$fecha_inicio' AND '$fecha_fin'
     LEFT JOIN registro_viajes rv ON rv.operador = CONCAT_WS(' ', e.nombres, e.apellido_paterno, e.apellido_materno)
         AND DATE(rv.fecha) BETWEEN '$fecha_inicio' AND '$fecha_fin'
         AND rv.valor_vuelta > 0";
