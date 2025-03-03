c8074fb7c26d19e4acdc57567bd17a709ba05d76
Raul Madero <raul.madero.ramirez@gmail.com>
2025-03-03 15:43:19 -0600

    Dias de vacaciones

--- sistema/data/nominaEmpleados.php
+++ sistema/data/nominaEmpleados.php
@@ -173,15 +173,7 @@ if(isset($_POST['semana']) && isset($_POST['anio']) && !empty($_POST['semana']) 
         END) AS sueldo_bruto,
             COALESCE(SUM(CASE 
                 WHEN inc.tipo_incidencia = 'Vacaciones' THEN 
-                    CASE 
-                        WHEN inc.fecha_inicial BETWEEN '$fecha_inicio' AND '$fecha_fin' THEN 
-                            DATEDIFF(LEAST(inc.fecha_final, '$fecha_fin'), inc.fecha_inicial) + 1
-                        WHEN inc.fecha_final BETWEEN '$fecha_inicio' AND '$fecha_fin' THEN 
-                            DATEDIFF(inc.fecha_final, GREATEST(inc.fecha_inicial, '$fecha_inicio')) + 1
-                        WHEN inc.fecha_inicial < '$fecha_inicio' AND inc.fecha_final > '$fecha_fin' THEN 
-                            DATEDIFF('$fecha_fin', '$fecha_inicio') + 1
-                        ELSE 0
-                    END
+                    DATEDIFF(LEAST(inc.fecha_final, '$fecha_fin'), GREATEST(inc.fecha_inicial, '$fecha_inicio')) + 1
                 ELSE 0
             END), 0) AS dias_vacaciones_pagar,
         (
