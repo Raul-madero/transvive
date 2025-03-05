c87807eb3a597ca1571ce2d5a70ba2c3cd0f49ac
Raul Madero <raul.madero.ramirez@gmail.com>
2025-03-05 13:39:51 -0600

    Sin actualizar nomina

--- sistema/data/updateSueldo.php
+++ sistema/data/updateSueldo.php
@@ -15,7 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $id = isset($_POST['id']) ? intval($_POST['id']) : 0;
     $sueldo = isset($_POST['sueldo']) ? floatval($_POST['sueldo']) : 0;
     $nomina_fiscal = isset($_POST['nomina_fiscal']) ? floatval($_POST['nomina_fiscal']) : 0;
-    echo $nomina_fiscal;
+    // echo $nomina_fiscal;
     $efectivo = $sueldo - $nomina_fiscal;
     // var_dump($_POST);
     // Validar que el ID sea válido
