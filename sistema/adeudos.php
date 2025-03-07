9ed60a3feed067e923e4a500cb5258d2897c41ff
Raul Madero <raul.madero.ramirez@gmail.com>
2025-03-07 15:34:18 -0600

    Registro

--- sistema/adeudos.php
+++ sistema/adeudos.php
@@ -168,6 +168,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                     }
                 }
             });
+			
         } else {
             console.error("Tabla #example1 no encontrada.");
         }
