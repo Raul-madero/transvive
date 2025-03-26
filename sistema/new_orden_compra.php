6d78e31ac9f741364a7fa090d24538afaae26930
Raul Madero <raul.madero.ramirez@gmail.com>
2025-03-26 14:15:57 -0600

    Scripts jquery

--- sistema/new_orden_compra.php
+++ sistema/new_orden_compra.php
@@ -290,7 +290,12 @@ $filasprov = mysqli_fetch_all($queryprov, MYSQLI_ASSOC);
 <!-- REQUIRED SCRIPTS -->
 
 <!-- jQuery -->
-<script src="../plugins/jquery/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+<!-- DataTables CSS y JS -->
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+
 <!-- Bootstrap 4 -->
 <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
