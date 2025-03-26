137bed70123ebd587c7bd697cca2a9d6d02eb6c1
Raul Madero <raul.madero.ramirez@gmail.com>
2025-03-26 14:47:27 -0600

    Llamada a tabla refacciones

--- sistema/data/detalles_requisicion.php
+++ sistema/data/detalles_requisicion.php
@@ -1,17 +1,32 @@
 <?php
 session_start();
 include ("../../conexion.php");
-if($conection->connect_error) {
-    die("Error de conexion a la base d edatos". $conection->connect_error);
+
+if ($conection->connect_error) {
+    die("Error de conexión a la base de datos: " . $conection->connect_error);
 }
 
-$folio_requisicion = isset($_POST['folio'] ) ? $_POST['folio'] : die("Error al obtener el folio de la requisicion");
+$folio_requisicion = isset($_POST['folio']) ? $_POST['folio'] : die("Error al obtener el folio de la requisición");
 
-$sql = "SELECT * FROM detalle_requisicioncompra WHERE folio = ?";
+$sql = "
+SELECT 
+  d.id,
+  d.cantidad,
+  d.codigo,
+  d.descripcion,
+  r.umedida AS unidad_medida,
+  d.marca,
+  d.precio,
+  (d.cantidad * d.precio) AS importe
+FROM detalle_requisicioncompra d
+LEFT JOIN refacciones r ON d.codigo = r.codigo
+WHERE d.folio = ?";
+
 $stmt = $conection->prepare($sql);
 if (!$stmt) {
     die("Error en prepare(): " . $conection->error);
 }
+
 $stmt->bind_param("i", $folio_requisicion);
 $stmt->execute();
 $result = $stmt->get_result();
@@ -22,5 +37,6 @@ while($row = $result->fetch_assoc()) {
 }
 
 echo json_encode(["data" => $data], JSON_UNESCAPED_UNICODE);
+
 $stmt->close();
 $conection->close();
