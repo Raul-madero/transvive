84f25f18e297d92048fdd448b11b0e7d76deb0dc
Raul Madero <raul.madero.ramirez@gmail.com>
2025-03-28 14:44:18 -0600

    Seleccion codigo interno refacciones

--- sistema/new_cotizacioncompra.php
+++ sistema/new_cotizacioncompra.php
@@ -33,7 +33,7 @@ $sqlprod   = "select id, codigo, descripcion, marca from refacciones where estat
 $queryprod = mysqli_query($conection, $sqlprod);
 $filasprod = mysqli_fetch_all($queryprod, MYSQLI_ASSOC);
 
-$sqlprodnm = "select id, codigo, descripcion, marca from refacciones where estatus = 1 ORDER BY descripcion";
+$sqlprodnm = "select id, codigo, codigo_interno, descripcion, marca from refacciones where estatus = 1 ORDER BY descripcion";
 $queryprodnm = mysqli_query($conection, $sqlprodnm);
 $filasprodnm = mysqli_fetch_all($queryprodnm, MYSQLI_ASSOC);
 
@@ -464,7 +464,7 @@ $(document).ready(function () {
                                 <select class="form-control select2bs4" style="width: 100%; text-align: left" id="inputDescripcion" name="inputDescripcion">
                                     <option value="">- Seleccione -</option>
                                     <?php foreach ($filasprodnm as $opnm): //llenar las opciones del primer select ?>
-                                    <option value="<?= $opnm['descripcion'] ?>"><?= $opnm['descripcion'] ?></option>  
+                                    <option value="<?= $opnm['descripcion'] ?>"><?= $opm['codigo_interno'] . '-' . $opnm['descripcion'] ?></option>  
                                     <?php endforeach; ?>
                                 </select>
                             </div>
