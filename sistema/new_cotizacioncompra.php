fe42f53ffc3a1243e332c57577b3c910c94c3056
Raul Madero <raul.madero.ramirez@gmail.com>
2025-04-01 12:48:00 -0600

    Tamano tabla

--- sistema/new_cotizacioncompra.php
+++ sistema/new_cotizacioncompra.php
@@ -242,13 +242,13 @@ mysqli_close($conection);
                                 <table class="table table-bordered table-hover">
                                     <thead>
                                         <tr>
-                                            <th style="width:5%; background-color:#e9ecef; text-align: center;" >Cantidad</th>
+                                            <th style="width:10%; background-color:#e9ecef; text-align: center;" >Cantidad</th>
                                             <th style="width:10%; background-color:#e9ecef; text-align: center;" >Codigo</th>
-                                            <th style="width:30%; background-color:#e9ecef; text-align: center;">Descripción</th>
+                                            <th style="width:35%; background-color:#e9ecef; text-align: center;">Descripción</th>
                                             <th style="width:15%; background-color:#e9ecef; text-align: center;" >Marca</th>
                                             <th style="width:5%; background-color:#e9ecef; text-align: center;" >E</th>
                                             <th style="width:5%; background-color:#e9ecef; text-align: center;" >OM</th>
-                                            <th style="width:25%; background-color:#e9ecef; text-align: center;" >Precio Unitario</th>
+                                            <th style="width:15%; background-color:#e9ecef; text-align: center;" >Precio Unitario</th>
                                             <th style="width:5%; background-color:#e9ecef; text-align: center;">Acciones</th>
                                         </tr>
                                     </thead>
