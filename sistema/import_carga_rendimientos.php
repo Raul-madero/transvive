31466460eece383ff115209136e268a4186e37f9
Raul Madero <raul.madero.ramirez@gmail.com>
2025-04-07 16:25:12 -0600

    Sin fecha

--- sistema/import_carga_rendimientos.php
+++ sistema/import_carga_rendimientos.php
@@ -30,7 +30,7 @@ if (isset($_FILES['name']) && $_FILES['name']['error'] == 0) {
                     $stmt = $conection->prepare("UPDATE carga_combustible SET rendimiento_estandar = ? WHERE nounidad = ? AND MONTH(fecha) = MONTH(CURRENT_DATE()) AND YEAR(fecha) = YEAR(CURRENT_DATE())");
                     $stmt->bind_param("ds", $rendimiento, $no_unidad);
 
-                    $stmt2 = $conection->prepare("UPDATE unidades SET rendimiento_estandar = ? WHERE no_unidad = ? AND MONTH(fecha) = MONTH(CURRENT_DATE()) AND YEAR(fecha) = YEAR(CURRENT_DATE())");
+                    $stmt2 = $conection->prepare("UPDATE unidades SET rendimiento_estandar = ? WHERE no_unidad = ?");
                     $stmt2->bind_param("ds", $rendimiento, $no_unidad);
 
                     if ($stmt->execute() && $stmt->affected_rows > 0) {
