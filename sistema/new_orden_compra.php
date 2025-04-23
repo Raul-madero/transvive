5122af58e8a08741b633652078f6a18633d24a9f
Raul Madero <raul.madero.ramirez@gmail.com>
2025-04-23 10:16:33 -0600

    Resta de impuesto

--- sistema/new_orden_compra.php
+++ sistema/new_orden_compra.php
@@ -379,7 +379,7 @@ $filasrecb = mysqli_fetch_all($queryrecb, MYSQLI_ASSOC);
 
     impuestosAdicionales.forEach((imp, idx) => {
       const monto = subtotal * (imp.porcentaje / 100);
-      if(imp.tipo === 'ISR') {
+      if(imp.tipo === 'ISR' || imp.tipo === 'Retencion de IVA') {
         total -= monto; // ISR se resta del total
       }else {
         total += monto; // Otros impuestos se suman al total
