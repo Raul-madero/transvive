db2637a58c0baeadd1f166c43049829e1bbf2e35
Raul Madero <raul.madero.ramirez@gmail.com>
2025-06-29 21:24:43 -0600

    JAC

--- sistema/data/get_envios3.php
+++ sistema/data/get_envios3.php
@@ -22,9 +22,9 @@ if ($banda_id3 == 'Camion') {
 } else {
   if ($banda_id3 == 'Camioneta') {
     $sql = "select * FROM unidades where no_unidad like '%T-%' ORDER BY id";  
-  $query = mysqli_query($conection, $sql);
-  $filas = mysqli_fetch_all($query, MYSQLI_ASSOC); 
-  mysqli_close($conection);
+    $query = mysqli_query($conection, $sql);
+    $filas = mysqli_fetch_all($query, MYSQLI_ASSOC); 
+    mysqli_close($conection);
 
 ?>
 
@@ -33,12 +33,12 @@ if ($banda_id3 == 'Camion') {
 <option value="<?= $op['no_unidad'] ?>"><?= $op['no_unidad'] ?></option>
 <?php endforeach; ?>
 <?php
-  }else{
+  } else  {
     if ($banda_id3 == 'Automovil') {
-  $sql = "select * FROM unidades where no_unidad like '%A-%' ORDER BY id";  
-  $query = mysqli_query($conection, $sql);
-  $filas = mysqli_fetch_all($query, MYSQLI_ASSOC); 
-  mysqli_close($conection);
+      $sql = "select * FROM unidades where no_unidad like '%A-%' ORDER BY id";  
+      $query = mysqli_query($conection, $sql);
+      $filas = mysqli_fetch_all($query, MYSQLI_ASSOC); 
+      mysqli_close($conection);
 
 ?>
 
@@ -50,9 +50,9 @@ if ($banda_id3 == 'Camion') {
     }else {
       if ($banda_id3 == 'Sprinter') {
         $sql = "select * FROM unidades where no_unidad like '%S-%' ORDER BY id";  
-  $query = mysqli_query($conection, $sql);
-  $filas = mysqli_fetch_all($query, MYSQLI_ASSOC); 
-  mysqli_close($conection);
+        $query = mysqli_query($conection, $sql);
+      $filas = mysqli_fetch_all($query, MYSQLI_ASSOC); 
+      mysqli_close($conection);
 
 ?>
 
@@ -64,9 +64,9 @@ if ($banda_id3 == 'Camion') {
       }else {
         if ($banda_id3 == 'Unidad Externa') {
         $sql = "select * FROM unidades where no_unidad like '%Externa%' ORDER BY id";  
-  $query = mysqli_query($conection, $sql);
-  $filas = mysqli_fetch_all($query, MYSQLI_ASSOC); 
-  mysqli_close($conection);
+        $query = mysqli_query($conection, $sql);
+        $filas = mysqli_fetch_all($query, MYSQLI_ASSOC); 
+        mysqli_close($conection);
 
 ?>
 
@@ -75,7 +75,19 @@ if ($banda_id3 == 'Camion') {
 <option value="<?= $op['no_unidad'] ?>"><?= $op['no_unidad'] ?></option>
 <?php endforeach; ?>
 <?php
-}
+        }else {
+          if ($banda_id3 == 'JAC') {
+            $sql = "select * FROM unidades where no_unidad like '%T-%' ORDER BY id";  
+            $query = mysqli_query($conection, $sql);
+            $filas = mysqli_fetch_all($query, MYSQLI_ASSOC); 
+            mysqli_close($conection);
+            ?>
+            <option value="">- Seleccione -</option>
+            <?php foreach($filas as $op): //creamos las opciones a partir de los datos obtenidos ?>
+            <option value="<?= $op['no_unidad'] ?>"><?= $op['no_unidad'] ?></option>
+            <?php endforeach; ?>
+            <?php
+        }
       }
     }
   }
