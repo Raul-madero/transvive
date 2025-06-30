7ee322ba38e04b7ec753f91112bf26052ff6423e
Raul Madero <raul.madero.ramirez@gmail.com>
2025-06-29 21:41:14 -0600

    JAC4

--- sistema/data/get_envios3.php
+++ sistema/data/get_envios3.php
@@ -1,102 +1,30 @@
 <?php
-include "../../conexion.php"; //libreria de conexion a la base
+include "../../conexion.php";
 
-//$banda_id = filter_input(INPUT_POST, 'banda_id'); //obtenemos el parametro que viene de ajax
-$banda_id3 = $_POST['banda_id3'];
-var_dump($_POST);
+$banda_id3 = $_POST['banda_id3'] ?? '';
 
-if ($banda_id3 == 'Camion') {
-  /*Obtenemos los discos de la banda seleccionada*/
-  $sql = "select * FROM unidades where no_unidad like '%C-%' ORDER BY id";
-  $query = mysqli_query($conection, $sql);
-  $filas = mysqli_fetch_all($query, MYSQLI_ASSOC);
-  mysqli_close($conection);
+// Asociaciones por tipo de unidad
+$tipos = [
+    'Camion'        => '%C-%',
+    'Camioneta'     => '%T-%',
+    'Automovil'     => '%A-%',
+    'Sprinter'      => '%S-%',
+    'Unidad Externa'=> '%Externa%',
+    'JAC'           => '%J-%'
+];
 
-?>
-
-  <option value="">- Seleccione -</option>
-  <?php foreach ($filas as $op): //creamos las opciones a partir de los datos obtenidos 
-  ?>
-    <option value="<?= $op['no_unidad'] ?>"><?= $op['no_unidad'] ?></option>
-  <?php endforeach; ?>
-  <?php
-} else {
-  if ($banda_id3 == 'Camioneta') {
-    $sql = "select * FROM unidades where no_unidad like '%T-%' ORDER BY id";
+// Validamos si el tipo existe en el arreglo
+if (array_key_exists($banda_id3, $tipos)) {
+    $like = $tipos[$banda_id3];
+    $sql = "SELECT * FROM unidades WHERE no_unidad LIKE '$like' ORDER BY id";
     $query = mysqli_query($conection, $sql);
     $filas = mysqli_fetch_all($query, MYSQLI_ASSOC);
     mysqli_close($conection);
-
-  ?>
-
-    <option value="">- Seleccione -</option>
-    <?php foreach ($filas as $op): //creamos las opciones a partir de los datos obtenidos 
-    ?>
-      <option value="<?= $op['no_unidad'] ?>"><?= $op['no_unidad'] ?></option>
-    <?php endforeach; ?>
-    <?php
-  } else {
-    if ($banda_id3 == 'Automovil') {
-      $sql = "select * FROM unidades where no_unidad like '%A-%' ORDER BY id";
-      $query = mysqli_query($conection, $sql);
-      $filas = mysqli_fetch_all($query, MYSQLI_ASSOC);
-      mysqli_close($conection);
-
-    ?>
-
-      <option value="">- Seleccione -</option>
-      <?php foreach ($filas as $op): //creamos las opciones a partir de los datos obtenidos 
-      ?>
-        <option value="<?= $op['no_unidad'] ?>"><?= $op['no_unidad'] ?></option>
-      <?php endforeach; ?>
-      <?php
-    } else {
-      if ($banda_id3 == 'Sprinter') {
-        $sql = "select * FROM unidades where no_unidad like '%S-%' ORDER BY id";
-        $query = mysqli_query($conection, $sql);
-        $filas = mysqli_fetch_all($query, MYSQLI_ASSOC);
-        mysqli_close($conection);
-
-      ?>
-
-        <option value="">- Seleccione -</option>
-        <?php foreach ($filas as $op): //creamos las opciones a partir de los datos obtenidos 
-        ?>
-          <option value="<?= $op['no_unidad'] ?>"><?= $op['no_unidad'] ?></option>
-        <?php endforeach; ?>
-        <?php
-      } else {
-        if ($banda_id3 == 'Unidad Externa') {
-          $sql = "select * FROM unidades where no_unidad like '%Externa%' ORDER BY id";
-          $query = mysqli_query($conection, $sql);
-          $filas = mysqli_fetch_all($query, MYSQLI_ASSOC);
-          mysqli_close($conection);
-
-        ?>
-
-          <option value="">- Seleccione -</option>
-          <?php foreach ($filas as $op): //creamos las opciones a partir de los datos obtenidos 
-          ?>
-            <option value="<?= $op['no_unidad'] ?>"><?= $op['no_unidad'] ?></option>
-          <?php endforeach; ?>
-          <?php
-        } else {
-          if ($banda_id3 == 'JAC') {
-            $sql = "select * FROM unidades where no_unidad like '%J-%' ORDER BY id";
-            $query = mysqli_query($conection, $sql);
-            $filas = mysqli_fetch_all($query, MYSQLI_ASSOC);
-            mysqli_close($conection);
-          ?>
-            <option value="">- Seleccione -</option>
-            <?php foreach ($filas as $op): //creamos las opciones a partir de los datos obtenidos 
-            ?>
-              <option value="<?= $op['no_unidad'] ?>"><?= $op['no_unidad'] ?></option>
-            <?php endforeach; ?>
-<?php
-          }
-        }
-      }
+    
+    // Generar las opciones del <select>
+    echo '<option value="">- Seleccione -</option>';
+    foreach ($filas as $op) {
+        echo '<option value="' . $op['no_unidad'] . '">' . $op['no_unidad'] . '</option>';
     }
-  }
 }
 ?>
