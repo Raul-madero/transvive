1bf2833f6a5b63a29d56c7ba4daf56a37856733d
Raul Madero <raul.madero.ramirez@gmail.com>
2025-07-31 09:06:37 -0600

    Totales

--- sistema/factura/recibo_nomina.php
+++ sistema/factura/recibo_nomina.php
@@ -97,22 +97,29 @@ function generarReciboSemanal($pdf, $conection, $semanaTexto, $anio) {
         $pdf->Cell(60, 5, 'Deduccion Fiscal: $' . number_format($row['deduccion_fiscal'], 2), 0, 1);
 
         $pdf->Cell(60, 5, 'Sueldo Adicional: $' . number_format($row['sueldo_adicional'], 2), 0, 0);
-        $pdf->Ln(6);
+        $pdf->Ln(5);
         $pdf->Cell(60, 5, 'Caja de Ahorro: $' . number_format($row['caja_ahorro'], 2), 0, 0);
-        $pdf->Ln(6);
+        $pdf->Ln(5);
         $pdf->Cell(60, 5, 'Bono Semanal: $' . number_format($row['bono_categoria'], 2), 0, 0);
-        $pdf->Ln(6);
+        $pdf->Ln(5);
         $pdf->Cell(60, 5, 'Bono Supervisor: $' . number_format($row['bono_supervisor'], 2), 0, 0);
-        $pdf->Ln(6);
+        $pdf->Ln(5);
         $pdf->Cell(60, 5, 'Bono Alertas: $' . number_format($row['bono_semanal'], 2), 0, 0);
-        $pdf->Ln(6);
+        $pdf->Ln(5);
         $pdf->Cell(60, 5, 'Apoyo Mensual: $' . number_format($row['apoyo_mes'], 2), 0, 0);
-        $pdf->Ln(6);
+        $pdf->Ln(5);
         $pdf->Cell(60, 5, 'Dias de Vacaciones: ' . number_format($row['dias_vacaciones'], 2), 0, 0);
-        $pdf->Ln(6);
+        $pdf->Ln(5);
         $pdf->Cell(60, 5, 'Pago Vacaciones: $' . number_format($row['pago_vacaciones'], 2), 0, 0);
-        $pdf->Ln(6);
+        $pdf->Ln(5);
         $pdf->Cell(60, 5, 'Prima Vacacional: $' . number_format($row['prima_vacacional'], 2), 0, 0);
+
+        $pdf->Ln(10);
+        $pdf->Cell(189, 5, utf8_decode('Total Percepciones: $' . number_format($row['sueldo_bruto'] + $row['bono_semanal'] + $row['bono_supervisor'] + $row['bono_categoria'] + $row['sueldo_adicional'] + $row['apoyo_mes'], 2)), 0, 1, 'C');
+        $pdf->Ln(5);
+        $pdf->Cell(60, 5,'Total Deducciones: $' . number_format($row['deducciones'], 2), 0, 1);
+        $pdf->Ln(5);
+        $pdf->Cell(60, 5,'Total Neto: $' . number_format($row['sueldo_bruto'] + $row['bono_semanal'] + $row['bono_supervisor'] + $row['bono_categoria'] + $row['sueldo_adicional'] + $row['apoyo_mes'] - $row['deducciones'], 2), 0, 1);
 
         $pdf->Ln(10);
         $pdf->Cell(189, 5, utf8_decode('Recibí conforme.'), 0, 1, 'L');
